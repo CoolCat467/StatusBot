57fe96c518597719f74e4d40226118c4922462fb
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-07-02 16:30:36 -0500

    Make it actually loop

--- looprun.sh
+++ looprun.sh
@@ -1,10 +1,4 @@
 #!/bin/env bash
 
-cd ~/Desktop/Bots
-python3 -m venv StatusBot
-cd StatusBot
-source bin/activate
-python3 -m pip install -r requirements.txt
-python3 bot.py
-deactivate
-
+~/Desktop/Bots/StatusBot/run.sh
+~/Desktop/Bots/StatusBot/looprun.sh
