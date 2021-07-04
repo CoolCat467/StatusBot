bb622f5e15aef6721f8ebe381d8694592f67799b
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-07-04 11:56:10 -0600

    Update install_update.sh

--- install_update.sh
+++ install_update.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # Install new verion of bot
 
-echo "Installing updated bot..."
 cd ~/Desktop
 cd Bots
+echo "Moving current to old."
 mv StatusBot old
+echo "Installing updated bot..."
 git clone https://github.com/CoolCat467/StatusBot
+echo "Moving old credentials over..."""
 mv old/.env StatusBot/.env
 cd StatusBot
 chmod 755 *
-echo "Install complete."""
+echo "Install complete."
 . looprun.sh
