96e4279fa45f671610db8b724cbe53dfc1dc33cc
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-07-10 11:27:03 -0500

    Create __init__.py

---
+++ bot/status/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__all__ = ['address_tools', 'connection', 'pingers', 'server_status']
+
+from status import address_tools
+from status import connection
+from status import pingers
+from status import server_status
