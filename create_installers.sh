464bb791a181e06884a7ac1ee57d98b3fc6b8b6c
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-07-14 17:36:51 -0500

    v0.1.5

--- create_installers.sh
+++ create_installers.sh
@@ -71,14 +71,19 @@ sudo cat > install_update.sh << EOF
 # -*- coding: utf-8 -*-
 # Install new verion of bot
 
-cd $VInstallDir
-cd Bots
+cd $VInstallDir/Bots 
+echo "Attempting to remove old."
+if [ -d "$VInstallDir/Bots/old" ] ; then
+    rm old
+fi
 echo "Moving current to old."
 mv StatusBot old
 echo "Installing updated bot..."
 git clone https://github.com/CoolCat467/StatusBot
 echo "Moving old credentials over..."
 mv old/bot/.env StatusBot/bot/.env
+echo "Moving old config over..."
+mv old/bot/config StatusBot/bot/config
 cd StatusBot
 chmod 755 *
 echo "Install complete."
--- install_update.sh
+++ install_update.sh
@@ -2,14 +2,19 @@
 # -*- coding: utf-8 -*-
 # Install new verion of bot
 
-cd ~/Desktop
-cd Bots
+cd ~/Desktop/Bots 
+echo "Attempting to remove old."
+if [ -d "~/Desktop/Bots/old" ] ; then
+    rm old
+fi
 echo "Moving current to old."
 mv StatusBot old
 echo "Installing updated bot..."
 git clone https://github.com/CoolCat467/StatusBot
 echo "Moving old credentials over..."
 mv old/bot/.env StatusBot/bot/.env
+echo "Moving old config over..."
+mv old/bot/config StatusBot/bot/config
 cd StatusBot
 chmod 755 *
 echo "Install complete."
