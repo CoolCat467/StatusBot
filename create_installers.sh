48586b1928916a6487d4084b16e75704c434d091
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2021-08-07 22:47:48 -0500

    Fix a few things, including requirements

--- create_installers.sh
+++ create_installers.sh
@@ -2,18 +2,20 @@
 # -*- coding: utf-8 -*-
 # Programmed by CoolCat467
 
+VBot='StatusBot'
+VAuthor='CoolCat467'
 echo -n "Install Directory: "
 read VInstallDir
 
 
-sudo cat > run.sh << EOF
+cat > run.sh << EOF
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Run bot in a virtual environment
 
 cd $VInstallDir/Bots
-python3 -m venv StatusBot
-cd StatusBot
+python3 -m venv $VBot
+cd $VBot
 source bin/activate
 python3 -m pip install -r requirements.txt
 cd bot
@@ -22,29 +24,29 @@ deactivate
 EOF
 
 
-sudo cat > run-quick.sh << EOF
+cat > run-quick.sh << EOF
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Run bot in a virtual environment without trying to reinstall requirements.
 
-cd $VInstallDir/Bots/StatusBot
+cd $VInstallDir/Bots/$VBot
 source bin/activate
 python3 bot.py
 deactivate
 EOF
 
 
-sudo cat > looprun.sh << EOF
+cat > looprun.sh << EOF
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Loop run.sh
 
-$VInstallDir/Bots/StatusBot/run.sh
-$VInstallDir/Bots/StatusBot/looprun.sh
+$VInstallDir/Bots/$VBot/run.sh
+$VInstallDir/Bots/$VBot/looprun.sh
 EOF
 
 
-sudo cat > install.sh << EOF
+cat > install.sh << EOF
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Install bot
@@ -53,8 +55,8 @@ cd $VInstallDir
 mkdir Bots
 cd Bots
 echo "Installing bot..."
-git clone https://github.com/CoolCat467/StatusBot
-cd StatusBot/bot
+git clone https://github.com/$VAuthor/$VBot
+cd $VBot/bot
 touch .env
 echo "# .env" > .env
 echo "DISCORD_TOKEN=" >> .env
@@ -66,7 +68,7 @@ echo "Install complete."
 EOF
 
 
-sudo cat > install_update.sh << EOF
+cat > install_update.sh << EOF
 #!/bin/bash
 # -*- coding: utf-8 -*-
 # Install new verion of bot
@@ -77,14 +79,14 @@ if [ -d "$VInstallDir/Bots/old" ] ; then
     rm old
 fi
 echo "Moving current to old."
-mv StatusBot old
+mv $VBot old
 echo "Installing updated bot..."
-git clone https://github.com/CoolCat467/StatusBot
+git clone https://github.com/$VAuthor/$VBot
 echo "Moving old credentials over..."
-mv old/bot/.env StatusBot/bot/.env
+mv old/bot/.env $VBot/bot/.env
 echo "Moving old config over..."
-mv old/bot/config StatusBot/bot/config
-cd StatusBot
+mv old/bot/config $VBot/bot/config
+cd $VBot
 chmod 755 *
 echo "Install complete."
 . looprun.sh
