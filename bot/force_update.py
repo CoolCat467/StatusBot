166acca4f106b06efb2a90ccddf65083116b088d
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2022-03-20 11:44:31 -0500

    Python 3.10.0 updates

--- bot/force_update.py
+++ bot/force_update.py
@@ -2,23 +2,24 @@
 # -*- coding: utf-8 -*-
 # Force update bot
 
-"""Force Update Bot"""
+"Force Update Bot"
 
 # Programmed by CoolCat467
 
 __title__ = 'Force update'
 __author__ = 'CoolCat467'
-__version__ = '0.0.0'
+__version__ = '0.0.1'
 __ver_major__ = 0
 __ver_minor__ = 0
-__ver_patch__ = 0
+__ver_patch__ = 1
 
-import os
+from os.path import split, abspath
 import asyncio
 import json
+
 import update
 
-ROOTDIR = os.path.split(os.path.abspath(__file__))[0]
+ROOTDIR = split(abspath(__file__))[0]
 
 async def get_github_file(path:str, timeout:int=10) -> str:
     "Return text from github file in this project decoded as utf-8"
@@ -26,7 +27,7 @@ async def get_github_file(path:str, timeout:int=10) -> str:
     return file.decode('utf-8')
 
 async def getonlinevers() -> tuple:
-    "Get online version, tell user in message.channel, and return said version as tuple."
+    "Return online version as tuple."
     # Get github version string
     version = await get_github_file('version.txt')
     # Send message about it.
@@ -34,7 +35,7 @@ async def getonlinevers() -> tuple:
     # Make it tuple and return it
     return tuple(map(int, version.strip().split('.')))
 
-async def update_files(timeout:int=20) -> None:
+async def update_files(timeout: int=20) -> None:
     "Preform update from github."
     # If we need update, get file list.
     print('Retrieving file list...')
@@ -48,15 +49,16 @@ async def update_files(timeout:int=20) -> None:
     # Get max amount of time this could take.
     maxtime = timeout*len(paths)
     # Tell user number of files we are updating.
-    print(f'{len(paths)} files will now be updated. ')
+    print(f'{len(paths)} files will now be updated.\n')
     # Update said files.
-    rootdir = os.path.split(ROOTDIR)[0]
+    rootdir = split(ROOTDIR)[0]
+    print('\n'.join(paths))
     await update.update_files(rootdir, paths, 'StatusBot', __author__, 'master', timeout)
-    print('Done.')
+    print('\nAll files in file list updated.')
 
-def run():
+def run() -> None:
     "Preform update."
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
     try:
         loop.run_until_complete(update_files())
     finally:
@@ -67,5 +69,5 @@ def run():
 
 
 if __name__ == '__main__':
-    print(f'{__title__} v{__version__}\nProgrammed by {__author__}.')
+    print(f'{__title__} v{__version__}\nProgrammed by {__author__}.\n')
     run()
