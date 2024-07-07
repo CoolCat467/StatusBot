5626049882406c7b651207189f1941c17365a73e
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-07-07 15:34:27 -0500

    Runner changes keep getting deleted on updates

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,11 @@
 # -*- coding: utf-8 -*-
 # Run bot in a virtual environment
 
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
 cd ~/Applications
 # python3.11 -m venv StatusBot
 cd StatusBot
